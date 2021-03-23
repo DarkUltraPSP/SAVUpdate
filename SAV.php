88f401b6f12df02d707c85e331a2f3b59a7005cc
Nero <darkultrapsp.mc@gmail.com>
2021-03-23 21:21:29 +0100

    Signed-off-by: Nero <darkultrapsp.mc@gmail.com>

---
+++ SAV.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Fiche SAV</title>
+        <link href="style.css" rel="stylesheet" type="text/css"  media="all"/>
+    </head>
+    <body>
+        <div class="page-container">
+            <div class="logo">
+                <img src="Update.png" class="logo">
+            </div>
+            <div class="form-">
+                
+            </div>
+        </div>
+    </body>
+</html>
